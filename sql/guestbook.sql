d45e67dbc7fb194bb3c263694bd88e1335352d1a
김현석 <gustjr324@gmail.com>
2022-05-25 17:46:02 +0900

    sql

--- sql/guestbook.sql
+++ sql/guestbook.sql
@@ -6,3 +6,4 @@ select no, name, password, message, reg_date from guestbook;
 insert into guestbook values (null,'김','1234','하하',now());
 
 delete from guestbook where no = 4 and password = '1234';
+
