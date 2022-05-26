29942eff2f0ed7c87c563eb13a29c78d357d8a4c
김현석 <gustjr324@gmail.com>
2022-05-26 13:06:55 +0900

    cookie #1

---
+++ helloweb/src/main/java/helloweb/CookieServlet.java
@@ -0,0 +1,46 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String COOKIENAME = "visitCount";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int visitCount = 0;
+
+		// 쿠키 읽기
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null && cookies.length > 0) {
+			for (Cookie cookie : cookies) {
+				if (COOKIENAME.equals(cookie.getName())) {
+					visitCount = Integer.parseInt(cookie.getValue());
+				}
+			}
+		}
+		visitCount++;
+		// 쿠키 쓰기
+		Cookie cookie = new Cookie(COOKIENAME, String.valueOf(visitCount));
+		cookie.setPath(request.getContextPath());
+		cookie.setMaxAge(24 * 60 * 60); // 1day 초단위 세팅이라 60 1분 60 1시간 24 1일
+		response.addCookie(cookie);
+		// 응답
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>방문 횟수: " + visitCount + "</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
